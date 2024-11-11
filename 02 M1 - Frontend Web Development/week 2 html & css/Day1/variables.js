6a8df5345ecdc9d174393415e98f579b4313471f
NocandaZen <zenandenocanda16@gmail.com>
2024-11-11 11:51:54 +0200

    add new js

---
+++ 02 M1 - Frontend Web Development/week 2 html & css/Day1/variables.js
@@ -0,0 +1,15 @@
+let myString = "Something"
+//shows the length on the string
+let length = myString.length
+
+//console.log(myString.length);
+
+//makes letter turn to lowercase -->myString.toLowercase
+//makes letters all turn to UPPERCASE -->myString.toUppercase
+console.log(myString.toLowerCase);
+
+let split = myString.split('')
+console.log(split);
+
+
+
---
+++ 02 M1 - Frontend Web Development/week 3 JavaScript/Exercise 2/Exer2.Z.Nocanda.js
@@ -0,0 +1,23 @@
+//1
+let num1 = 2
+ //2
+let num2 = 4
+//3
+let num3 = num1.toString();
+console.log(typeof num3)
+//4
+let sum = num1+num2
+//console.log(sum);
+//5
+let product = num1*num2
+//console.log(product);
+//6
+console.log(sum)
+console.log(product);
+
+
+
+
+
+
+
